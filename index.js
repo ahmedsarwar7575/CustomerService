37615879e7f58da4dcb633cb0e6d7ee465df8a2e
ahmedsarwar7575 <ahmedsarwar7575@gmail.com>
2025-09-08 09:56:41 +0500

    Recording 03

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import seedDatabase from "./config/seed.js";
-import { debug } from "./inbound/recording.js";
+// import { debug } from "./inbound/recording.js";
 import http from "http";
 import { attachMediaStreamServer } from "./inbound/mediaStream.js";
 dotenv.config();
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 attachMediaStreamServer(server);
 server.listen(PORT, () => {
   // seedDatabase()
-  debug()
+  // debug()
   console.log(`Server running on port ${PORT}`);
   console.log(`API Documentation: http://localhost:${PORT}/local-test.html`);
 });
