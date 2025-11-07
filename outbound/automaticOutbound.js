4529bd8b7da298b6db6f0a41393b8a91dd17fbb5
ahmedsarwar7575 <ahmedsarwar7575@gmail.com>
2025-11-07 18:42:30 +0500

    Final 7 Nov

--- outbound/automaticOutbound.js
+++ outbound/automaticOutbound.js
@@ -6,7 +6,7 @@ dotenv.config();
 import User from "../models/user.js";
 import { makeSystemMessage } from "./prompt.js";
 import Call from "../models/Call.js";
-import processCallOutcome from "./processCallOutcome.js";
+import processCallOutcome from "./summerize.js";
 const {
   OPENAI_API_KEY,
   REALTIME_VOICE = "alloy",
