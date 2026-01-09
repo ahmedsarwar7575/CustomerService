fb66e03de5b65c7b87359c53fcf428f95095e0dd
ahmedsarwar7575 <ahmedsarwar7575@gmail.com>
2026-01-09 23:27:19 +0500

    Cader

--- abc.js
+++
@@ -1,8 +0,0 @@
-function isGoodbye(text = "") {
-    const t = text.toLowerCase();
-    return (
-      t.includes("goodbye") ||
-      /\bbye\b/.test(t)
-    );
-  }
-  console.log(isGoodbye("goodbye"));
